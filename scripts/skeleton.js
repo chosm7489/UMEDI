51b2c8aa496b58917ccf3a36f6aff9c960d3d9fb
Helen Liu <mail2helenl@gmail.com>
2022-10-31 12:23:30 -0700

    created main.html, main.js, skeleton.js, nav.html, footer.html; page seen after user logs in

---
+++ scripts/skeleton.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+// Loads the parts of skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#footerPlaceholder').load('./text/footer.html'));
+}
+loadSkeleton();  //invoke the function
