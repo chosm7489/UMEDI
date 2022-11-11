6ab581b688584e8b03743bacb3f692a4cf24d3c0
Sungmok Cho <chosm7489@gmail.com>
2022-11-11 14:06:03 -0800

    change nav bar & add space for edit and delete alaram setting

--- scripts/alarmclock.js
+++ scripts/alarmclock.js
@@ -56,9 +56,6 @@ function setAlarm() {
     }
 
     var time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
-    // if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
-    //     return alert("Please, select a valid time to set Alarm!");
-    // }
     alarmTime = time;
     isAlarmSet = true;
     content.classList.add("disable");
